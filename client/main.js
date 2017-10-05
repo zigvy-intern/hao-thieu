af9936133106ad00f59be9c8651e7b4f3e68e95c
Thieu <tqthieu96@gmail.com>
2017-10-05 11:47:22 +0700

    update main.js file in client folder

--- client/main.js
+++ client/main.js
@@ -1,7 +1,21 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import { createApp } from 'mantra-core';
+import initContext from './configs/context';
+//modules
+import coreModule from './modules/core';
+import usersModule from './modules/users';
+import itemsModule from './modules/items'
 import './main.html';
+
+//init context
+const context = initContext();
+//create app
+const app = createApp(context);
+app.loadModule(coreModule);
+app.loadModule(usersModule);
+app.loadModule(itemsModule);
+app.init();
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0

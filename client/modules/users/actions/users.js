7c828703ba9004399615156547fb3ba0bc450187
Thieu <tqthieu96@gmail.com>
2017-10-12 08:33:22 +0700

    add register

---
+++ client/modules/users/actions/users.js
@@ -0,0 +1,13 @@
+export default {
+  create({Meteor, LocalState}, email, password) {
+      if (!email) {
+        return LocalState.set('CREATE_USER_ERROR', 'Email is required.');
+      }
+      if (!password) {
+        return LocalState.set('CREATE_USER_ERROR', 'Password is required.');
+      }
+      LocalState.set('CREATE_USER_ERROR', null);
+      Accounts.createUser({email, password});
+      FlowRouter.go('/');
+    }
+};

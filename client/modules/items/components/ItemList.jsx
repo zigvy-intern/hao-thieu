6418a3114f7deb9cdce543ca0d040433e8725b69
Thieu <tqthieu96@gmail.com>
2017-10-17 08:55:18 +0700

    Fixed Item Progress 2

--- client/modules/items/components/ItemList.jsx
+++ client/modules/items/components/ItemList.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import Item from './Item.jsx';
 import { Row, Col, Glyphicon } from 'react-bootstrap';
 import ItemProgress from './ItemProgress.jsx';
-const ItemList = ({items}) => (
+
+const ItemList = ({items, percentage}) => (
+
 <Row>
+  <ItemProgress percentage={percentage} />
   <Row>
     <Col xs={12} sm={6}>
       <ItemProgress />
@@ -18,3 +21,19 @@ const ItemList = ({items}) => (
 </Row>
 );
 export default ItemList;
+/*const ItemList = ({items}) => (
+<Row>
+  <Row>
+    <Col xs={12} sm={6}>
+      <ItemProgress />
+    </Col>
+  </Row>
+  <Row className="show-grid">
+    <Col xs={12}>
+      <a href="/edit"><Glyphicon glyph="plus"></Glyphicon> New Item</a>
+    </Col>
+    <Item />
+  </Row>
+</Row>
+);
+export default ItemList;*/
--- client/modules/items/components/ItemProgress.jsx
+++ client/modules/items/components/ItemProgress.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { ProgressBar } from 'react-bootstrap';
-const now = 70;
+const ItemProgress = ({percentage}) => (
+<ProgressBar now={percentage} label="%(percent)s%"></ProgressBar>
+);
+export default ItemProgress;
+
+//Hao
+/*const now = 70;
 const ItemProgress = ({content}) => (
 <ProgressBar now={now} label={`${now}%`} />
 );
 
-export default ItemProgress;
+export default ItemProgress;*/
 
 /*import React from 'react';
 import { ProgressBar } from 'react-bootstrap';
